51c47456afc4691b4245eadd6f69ff7b4961fde7
David Funck <xdavidfunckx@gmail.com>
2022-04-07 13:28:44 +0200

    init push

--- restaurang/src/index.tsx
+++ restaurang/src/index.tsx
@@ -1,16 +1,31 @@
-import React from 'react';
-import ReactDOMClient from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOMClient from "react-dom/client";
+import "./index.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import reportWebVitals from "./reportWebVitals";
+import { Main } from "./components/main/main";
+import { Contact } from "./components/contact/contact";
+import App from "./App";
+import { NotFound } from "./components/notfound/notfound";
+import { Layout } from "./components/layout/layout";
+
 
 const container = document.getElementById("root");
 const root = ReactDOMClient.createRoot(container as Element);
 
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<App />} />
+          <Route path="/animals" element={<Main />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
