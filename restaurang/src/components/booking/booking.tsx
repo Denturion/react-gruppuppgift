fa384e2fd394be735b71601c218ae8811eb7200b
Rebecka Larsson <94102338+Reebeecka@users.noreply.github.com>
2022-04-08 16:25:21 +0200

    created some mockBookings

--- restaurang/src/components/booking/booking.tsx
+++ restaurang/src/components/booking/booking.tsx
@@ -1,5 +1,6 @@
 import { BookingCalendar } from "./bookingcalendar";
 import { BookingForm } from "./bookingform";
+import { CreateMockBooking } from "./createmockbooking";
 
 export function Booking () {
     return (
