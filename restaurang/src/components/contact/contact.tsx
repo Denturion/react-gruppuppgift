50d85044fca495ec7a6477c6029ab2badb6a9a87
Henrietta Lybeck <52459904+hellohely@users.noreply.github.com>
2022-04-08 21:02:12 +0200

    Added useState and useEffect. Not working
    Getting stuck on a type 'never" error

--- restaurang/src/components/contact/contact.tsx
+++ restaurang/src/components/contact/contact.tsx
@@ -1,4 +1,5 @@
 import axios from "axios"
+import { useEffect, useState } from "react";
 
 interface IRestaurant {
     _id: string,
@@ -10,23 +11,23 @@ interface IRestaurant {
 
 export function Contact () {
 
-    axios
-        .get<IRestaurant[]>(
-            "https://school-restaurant-api.azurewebsites.net/restaurant/624ff35c138a40561e115f1e"
-        )
-        .then((response => {
-            response.data.map((restaurant: IRestaurant) => {
-                console.log(restaurant.name);
-                
-                
-            })
-        }));
+const [restaurantInfo, setRestaurantinfo] = useState();
 
-        
+const getRestaurant = () => {
+    axios.get("https://school-restaurant-api.azurewebsites.net/restaurant/624ff35c138a40561e115f1e")
+    .then((response) => {
+        const apiRestaurant = response.data;
+        setRestaurantinfo(apiRestaurant[0]);
+    });
+};
+
+    useEffect(() => getRestaurant(), []);
+
+    console.log(restaurantInfo);
 
     return (
-    <div>
-        <h1>contact works!</h1>
-        <p>{ }</p>
-        </div>)
+        <div>
+            <p>Contacts works</p>
+            </div>
+   )
 }
