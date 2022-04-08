26708e6b8628e02156660b7a8574de12e6349e4b
Henrietta Lybeck <52459904+hellohely@users.noreply.github.com>
2022-04-08 19:17:46 +0200

    Started with contact page, not done
    Got restaurant info from API, need to present it on the page.

--- restaurang/src/components/contact/contact.tsx
+++ restaurang/src/components/contact/contact.tsx
@@ -1,6 +1,32 @@
-export function Contact () {
-    return (<>
-        </>)
-    
+import axios from "axios"
 
+interface IRestaurant {
+    _id: string,
+    name: string,
+    address: string,
+    zip: string,
+    city: string,
+}
+
+export function Contact () {
+
+    axios
+        .get<IRestaurant[]>(
+            "https://school-restaurant-api.azurewebsites.net/restaurant/624ff35c138a40561e115f1e"
+        )
+        .then((response => {
+            response.data.map((restaurant: IRestaurant) => {
+                console.log(restaurant.name);
+                
+                
+            })
+        }));
+
+        
+
+    return (
+    <div>
+        <h1>contact works!</h1>
+        <p>{ }</p>
+        </div>)
 }
