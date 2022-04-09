eb7ad679db11b423b627a21a6b7457aad62e0bf2
Henrietta Lybeck <52459904+hellohely@users.noreply.github.com>
2022-04-09 19:04:18 +0200

    Imported interface and created setState variable with array with an empty booking object

--- restaurang/src/components/admin/admin.tsx
+++ restaurang/src/components/admin/admin.tsx
@@ -1,4 +1,12 @@
+import { useState } from "react";
+import { IBooking } from "../interfaces/IBooking";
+
 export function Admin () {
+    const [bookingList, setBookingList] = useState<IBooking[]>([{id: "", restaurantId: "", date: "", time: "", numberOfGuests: 0, customerId: ""}]);
+
+
+    //Länk till restaurangens bokningar
+    //https://school-restaurant-api.azurewebsites.net/booking/restaurant/624ff35c138a40561e115f1e
     return 
         (<>
         </>)
