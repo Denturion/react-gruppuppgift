52254e2038e1e6b160134ce4b7b936473261b28a
Henrietta Lybeck <52459904+hellohely@users.noreply.github.com>
2022-04-10 14:46:43 +0200

    Set up route to admin, did axios from API and console logging bookings from API

--- restaurang/src/components/admin/admin.tsx
+++ restaurang/src/components/admin/admin.tsx
@@ -1,15 +1,25 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { IBooking } from "../interfaces/IBooking";
 
 export function Admin () {
     const [bookingList, setBookingList] = useState<IBooking[]>([{id: "", restaurantId: "", date: "", time: "", numberOfGuests: 0, customerId: ""}]);
 
+    const getBookings = () => {
+        axios.get<IBooking[]>("https://school-restaurant-api.azurewebsites.net/booking/restaurant/624ff35c138a40561e115f1e")
+    .then((response) => {
+        const apiBookings = response.data;
+        setBookingList(apiBookings);
+    });
+    };
 
-    //Länk till restaurangens bokningar
-    //https://school-restaurant-api.azurewebsites.net/booking/restaurant/624ff35c138a40561e115f1e
-    return 
-        (<>
-        </>)
+    useEffect(() => getBookings(), []);
+    console.log(bookingList);
+        
+
+    return(
+        <p>Admin works!</p>
+    )
         
 
 }
