15a1f4bb510910e396e98061d0f1f0776906624e
Henrietta Lybeck <52459904+hellohely@users.noreply.github.com>
2022-04-12 17:48:20 +0200

    Made interface for customer info. Created a foreach with an axios get, but its commented out because atm its an eternal loop :)

--- restaurang/src/components/admin/admin.tsx
+++ restaurang/src/components/admin/admin.tsx
@@ -2,6 +2,14 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { IBooking } from "../interfaces/IBooking";
 
+export interface ICustomerInfo {
+    id: string,
+    name: string,
+    lastname: string,
+    email: string,
+    phone: string,
+}
+
 export function Admin () {
     const [bookingList, setBookingList] = useState<IBooking[]>([{id: "", restaurantId: "", date: "", time: "", numberOfGuests: 0, customerId: ""}]);
 
@@ -13,12 +21,24 @@ export function Admin () {
     });
     };
 
-    useEffect(() => getBookings(), []);
-    console.log(bookingList);
-        
+    useEffect(() => {
+        getBookings()
+    }, []);
+
+    // Går igenom varje bokning i listan och gör en axios get. Just nu en oändlig loop
+   // bookingList.forEach(function(item, index, array) {
+        //axios.get(`https://school-restaurant-api.azurewebsites.net/customer/625044c93d77c8d9b3e011a4`)
+        //.then((response) => {
+            //console.log(response.data);
+            
+       // })
+   // });
 
     return(
+        <div>
         <p>Admin works!</p>
+        <p> { bookingList[0].date } </p>
+        </div>
     )
         
 
