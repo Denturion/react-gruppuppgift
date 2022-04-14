5902e0ce431195d133080a0e7efddc55819a5999
Rebecka Larsson <94102338+Reebeecka@users.noreply.github.com>
2022-04-14 13:44:28 +0200

    Push with number of Guests works
    Co-Authored-By: David Funck <92029798+Denturion@users.noreply.github.com>

--- restaurang/src/components/booking/postbooking.tsx
+++ restaurang/src/components/booking/postbooking.tsx
@@ -10,14 +10,16 @@ export function postBooking(time: string, mydate: string, guests: number, custom
     let restaurantID = "624ff35c138a40561e115f1e";
 
     let bookingToPost: IBookingCustomer = {
-        restautantID: restaurantID,
+        restaurantId: restaurantID,
         date: mydate,
         time: time,
         numberOfGuests: guests,
         customer: customer
     }
 
+    let numberOfTables = guests/6;
 
+    for (var i=numberOfTables; i > 0 ; i--){
         axios.post<IBooking>("https://school-restaurant-api.azurewebsites.net/booking/create", bookingToPost)
         .then(function(response){
             console.log(response);
@@ -25,7 +27,8 @@ export function postBooking(time: string, mydate: string, guests: number, custom
         .catch(function (error) {
             console.log(error);
           });
-
+          console.log(i);
+    }
     //När vi pushar, on number of Guest > 6 så måste vi pusha samma bokning två gånger
 
     return {};
