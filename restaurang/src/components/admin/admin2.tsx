11d522958d784fa54fe39f491aea8bb5e5544a45
Rebecka Larsson <94102338+Reebeecka@users.noreply.github.com>
2022-04-20 13:21:15 +0200

    Stucture

--- restaurang/src/components/admin/admin2.tsx
+++ restaurang/src/components/admin/admin2.tsx
@@ -5,6 +5,7 @@ import { useForm } from "react-hook-form";
 import { BookingCalendar } from "../booking/bookingcalendar";
 import { IBooking } from "../interfaces/IBooking";
 import { ICustomerAndBooking } from "../interfaces/ICustomerAndBooking";
+import { putSubmits } from "./putsubmits";
 
 
 export function Admin() {
@@ -43,112 +44,22 @@ export function Admin() {
     }
 
 
-    //UPPDATE BOOKING TAR DIG HIT
-    function useshowForm(info: ICustomerAndBooking, index: number) {
-        return (
-            <div>
-                <form onSubmit={handleSubmit((data) => {
-                    handleSubmits(data, index);
-                })}>
-
-                    <label htmlFor="firstName">Förnamn:</label>
-                    <input id="firstName" type="text"{...register("firstName", { required: "Du måste skriva här", minLength: { value: 2, message: "Måste vara minst 2 bokstäver" } })} defaultValue={info.customerData.name} />
-                    <p>{errors.firstName?.message}</p>
-
-                    <label htmlFor="lastName">Efternamn:</label>
-                    <input id="lastName" type="text"{...register("lastName", { required: "Du måste skriva här", minLength: { value: 2, message: "Måste vara minst 2 bokstäver" } })} defaultValue={info.customerData.lastname} />
-                    <p>{errors.firstName?.message}</p>
-
-                    <label htmlFor="email">E-post:</label>
-                    <input id="email" type="email"{...register("email", {
-                        required: "Du måste skriva här", pattern: {
-                            value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                            message: "Ogiltig epost adress"
-                        }, minLength: { value: 2, message: "Måste vara 3 bokstäver" }
-                    })} defaultValue={info.customerData.email} />
-                    <p>{errors.email?.message}</p>
-
-                    <label htmlFor="phone">Telefonnummer:</label>
-                    <input id="phone" type="text"{...register("phone", { required: "Du måste skriva här", pattern: { value: /^[0-9]+$/i, message: "Endast siffror" } })} defaultValue={info.customerData.phone} />
-                    <p>{errors.phone?.message}</p>
-
-                    <label htmlFor="date">Datum:</label>
-                    <p>Tänk på att dubbelkolla så att det finns bord innan du byter datum</p>
-                    <input id="date" type="text"{...register("date", { required: "Du måste skriva här", pattern: { value: /^\d{4}\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])$/, message: "Formatet för datumet måste vara yyyy-mm-dd" } })} defaultValue={info.booking.date} />
-                    <p>{errors.date?.message}</p>
-
-                    <label htmlFor="time">Tid:</label>
-                    <p>Tänk på att dubbelkolla så att det finns bord innan du byter tid</p>
-                    <select id="time" {...register("time")}>
-                        <option value="18.00">18.00</option>
-                        <option value="21-00">21.00</option>
-                    </select>
-
-                    <label htmlFor="guest">Antal Gäster:</label>
-                    <input id="guest" type="number"{...register("guest")} defaultValue={info.booking.numberOfGuests} />
-
-                    <input type="submit"></input>
-                </form>
-            </div>
-        )
-    }
-
 
     function handleSubmits(data: any, index: number) {
         setShow(9999999999999);
         let array = [...customerAndBooking];
 
-        array[index] = {
-            booking: {
-                _id: array[index].booking._id,
-                restaurantId: "624ff35c138a40561e115f1e",
-                date: data.date,
-                time: data.time,
-                numberOfGuests: data.guest,
-                customerId: array[index].booking.customerId
-            },
-            customerData: {
-                _id: array[index].customerData._id,
-                name: data.firstName,
-                lastname: data.lastName,
-                email: data.email,
-                phone: data.phone,
-            }
-        };
-        let updatedBookingToPutToAPI = {
-            id: array[index].booking._id,
-            restaurantId: array[index].booking.restaurantId,
-            date: array[index].booking.date,
-            time: array[index].booking.time,
-            numberOfGuests: array[index].booking.numberOfGuests,
-            customerId: array[index].booking.customerId
-        }
-        let updatedCustomerToPutToAPI = {
-            id: array[index].customerData._id,
-            name: array[index].customerData.name,
-            lastname: array[index].customerData.lastname,
-            email: array[index].customerData.email,
-            phone: array[index].customerData.phone
-        }
+        let newCustomerAndBooking = putSubmits(array, data, index);
 
-        axios.put<IBooking>("https://school-restaurant-api.azurewebsites.net/booking/update/" + updatedBookingToPutToAPI.id, updatedBookingToPutToAPI, { headers: { "content-type": "application/json" } })
-            .then(response => {
-                console.log(response);
-            })
-        axios.put<IBooking>("https://school-restaurant-api.azurewebsites.net/customer/update/" + updatedCustomerToPutToAPI.id, updatedCustomerToPutToAPI, { headers: { "content-type": "application/json" } })
-            .then(response => {
-                console.log(response);
-            })
-
-        setCustomerAndBooking(array);
+        setCustomerAndBooking(newCustomerAndBooking);
     }
 
 
     function updateBooking(index: number) {
-        if (show === index){
+        if (show === index) {
             setShow(9999999999999)
         }
-        else{
+        else {
             setShow(index);
         }
     };
@@ -169,6 +80,106 @@ export function Admin() {
         setShowBooking(true)
     }
 
+    //HTML//
+
+    function useshowForm(info: ICustomerAndBooking, index: number) {
+        return (
+            <div>
+                <form onSubmit={handleSubmit((data) => {
+                    handleSubmits(data, index);
+                })}>
+
+                    <label htmlFor="firstName">Förnamn:</label>
+                    <input id="firstName" type="text"{
+                        ...register("firstName",
+                            {
+                                required: "Du måste skriva här",
+                                minLength: {
+                                    value: 2,
+                                    message: "Måste vara minst 2 bokstäver"
+                                }
+                            }
+                        )
+                    }
+                        defaultValue={info.customerData.name} />
+                    <p>{errors.firstName?.message}</p>
+
+                    <label htmlFor="lastName">Efternamn:</label>
+                    <input id="lastName" type="text"{
+                        ...register("lastName",
+                            {
+                                required: "Du måste skriva här",
+                                minLength: {
+                                    value: 2,
+                                    message: "Måste vara minst 2 bokstäver"
+                                }
+                            }
+                        )
+                    }
+                        defaultValue={info.customerData.lastname} />
+                    <p>{errors.firstName?.message}</p>
+
+                    <label htmlFor="email">E-post:</label>
+                    <input id="email" type="email"{
+                        ...register("email",
+                            {
+                                required: "Du måste skriva här",
+                                pattern: {
+                                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                                    message: "Ogiltig epost adress"
+                                }
+                            }
+                        )
+                    }
+                        defaultValue={info.customerData.email} />
+                    <p>{errors.email?.message}</p>
+
+                    <label htmlFor="phone">Telefonnummer:</label>
+                    <input id="phone" type="text"{
+                        ...register("phone",
+                            {
+                                required: "Du måste skriva här",
+                                pattern: {
+                                    value: /^[0-9]+$/i,
+                                    message: "Endast siffror"
+                                }
+                            }
+                        )
+                    }
+                        defaultValue={info.customerData.phone} />
+                    <p>{errors.phone?.message}</p>
+
+                    <label htmlFor="date">Datum:</label>
+                    <p>Tänk på att dubbelkolla så att det finns bord innan du byter datum</p>
+                    <input id="date" type="text"{
+                        ...register("date",
+                            {
+                                required: "Du måste skriva här",
+                                pattern: {
+                                    value: /^\d{4}\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])$/,
+                                    message: "Formatet för datumet måste vara yyyy-mm-dd"
+                                }
+                            }
+                        )
+                    }
+                        defaultValue={info.booking.date} />
+                    <p>{errors.date?.message}</p>
+
+                    <label htmlFor="time">Tid:</label>
+                    <p>Tänk på att dubbelkolla så att det finns bord innan du byter tid</p>
+                    <select id="time" {...register("time")}>
+                        <option value="18.00">18.00</option>
+                        <option value="21-00">21.00</option>
+                    </select>
+
+                    <label htmlFor="guest">Antal Gäster:</label>
+                    <input id="guest" type="number"{...register("guest")} defaultValue={info.booking.numberOfGuests} />
+
+                    <input type="submit"></input>
+                </form>
+            </div>
+        )
+    }
 
     let lis = customerAndBooking.map((data, i) => {
         return (
@@ -191,8 +202,8 @@ export function Admin() {
                 {lis}
             </div>
             {showBooking &&
-
-                (showBooking && !submitCompleted) && <BookingCalendar submitComplete={submitComplete}></BookingCalendar>}
+                (showBooking && !submitCompleted) && <BookingCalendar submitComplete={submitComplete}></BookingCalendar>
+            }
             {submitCompleted && (
                 <div>
                     Din bokning är nu inskickad och visas nästa gång du kommer in på sidan!
