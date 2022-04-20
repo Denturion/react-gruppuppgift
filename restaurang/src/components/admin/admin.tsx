395ed357a174483c41ea6f113475bbeabf10cc62
Rebecka Larsson <94102338+Reebeecka@users.noreply.github.com>
2022-04-20 18:13:02 +0200

    Moved form out of admin to own tsx file

--- restaurang/src/components/admin/admin.tsx
+++ restaurang/src/components/admin/admin.tsx
@@ -6,6 +6,7 @@ import { IBooking } from "../interfaces/IBooking";
 import { ICustomerAndBooking } from "../interfaces/ICustomerAndBooking";
 import { putSubmits } from "./putsubmits";
 import "./css/admin.css";
+import { AdminForm } from "./adminform";
 
 
 export function Admin() {
@@ -17,9 +18,6 @@ export function Admin() {
     const [customer, setCustomer] = useState(9999999999999);
     const [showBooking, setShowBooking] = useState(false);
     const [submitCompleted, setSubmitCompleted] = useState(false);
-
-    //CONST FOR REACT HOOK FORMS    
-    const { register, handleSubmit, formState: { errors } } = useForm();
 
     useEffect(() => {
         getBookingAndCustomers();
@@ -117,116 +115,6 @@ export function Admin() {
 
     //CREATE FORM WITH VALIDATION
     function useshowForm(info: ICustomerAndBooking, index: number) {
-
-        console.log(info);
-
-        return (
-            <div>
-                <form onSubmit={handleSubmit((data) => {
-                    handleSubmits(data, index);
-                })}>
-
-                    <label htmlFor="firstName">Förnamn:</label>
-                    <input id="firstName" type="text"{
-                        ...register("firstName",
-                            {
-                                required: "Du måste skriva här",
-                                minLength: {
-                                    value: 2,
-                                    message: "Måste vara minst 2 bokstäver"
-                                }
-                            }
-                        )
-                    }
-                        defaultValue={info.customerData.name} />
-                    <p>{errors.firstName?.message}</p>
-
-                    <label htmlFor="lastName">Efternamn:</label>
-                    <input id="lastName" type="text"{
-                        ...register("lastName",
-                            {
-                                required: "Du måste skriva här",
-                                minLength: {
-                                    value: 2,
-                                    message: "Måste vara minst 2 bokstäver"
-                                }
-                            }
-                        )
-                    }
-                        defaultValue={info.customerData.lastname} />
-                    <p>{errors.lastName?.message}</p>
-
-                    <label htmlFor="email">E-post:</label>
-                    <input id="email" type="email"{
-                        ...register("email",
-                            {
-                                required: "Du måste skriva här",
-                                pattern: {
-                                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                                    message: "Ogiltig epost adress"
-                                }
-                            }
-                        )
-                    }
-                        defaultValue={info.customerData.email} />
-                    <p>{errors.email?.message}</p>
-
-                    <label htmlFor="phone">Telefonnummer:</label>
-                    <input id="phone" type="text"{
-                        ...register("phone",
-                            {
-                                required: "Du måste skriva här",
-                                pattern: {
-                                    value: /^[0-9]+$/i,
-                                    message: "Endast siffror"
-                                }
-                            }
-                        )
-                    }
-                        defaultValue={info.customerData.phone} />
-                    <p>{errors.phone?.message}</p>
-
-                    <label htmlFor="date">Datum:</label>
-                    <input id="date" type="text"{
-                        ...register("date",
-                            {
-                                required: "Du måste skriva här",
-                                pattern: {
-                                    value: /^\d{4}\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])$/,
-                                    message: "Formatet för datumet måste vara yyyy-mm-dd"
-                                }
-                            }
-                        )
-                    }
-                        defaultValue={info.booking.date} />
-                    <p>{errors.date?.message}</p>
-
-                    <label htmlFor="time">Tid:</label>
-                    <select id="time" {...register("time")}>
-                        <option value="18.00">18.00</option>
-                        <option value="21-00">21.00</option>
-                    </select>
-
-                    <label htmlFor="guest">Antal Gäster: </label>
-                    <span>1-12</span>
-                    <input id="guest" type="number"{
-                        ...register("guest",{
-                            min: {
-                                value: 1,
-                                message: "Minst 1 gäst"
-                            },
-                            max: {
-                                value: 12,
-                                message: "Max 12 gäster"
-                            }
-                        })
-                        } defaultValue={info.booking.numberOfGuests} />
-                        <p>{errors.guest?.message}</p>
-
-                    <button type="submit">Skicka</button>
-                </form>
-            </div>
-         )
     }
 
     //CREATE HTML FOR EACH CUSTOMER
@@ -254,7 +142,7 @@ export function Admin() {
                         <button onClick={() => { updateBooking(i) }}>Uppdatera bokning</button>
                     </>
                 }
-                {(show === i) && useshowForm(data, i)}
+                {(show === i) && <AdminForm info={data} index={i} handleSumbits={handleSubmits}></AdminForm>}
             </div>
         )
     });
